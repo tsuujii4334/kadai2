9013258e101210a056a964c5ce6aee511a8e3153
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-05-03 08:26:46 +0000

    4-13[Bookモデル制作から再開]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create'
   get 'books' => 'books#index'
+  post 'books' => 'books#create'
   get 'books/show'
   get 'books/edit'
   get 'homes/top'
